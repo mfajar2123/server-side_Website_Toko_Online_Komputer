aa516afc1d01c63a1e0bce41dc01e01a3fa5fcb7
Ilham <muhammadilhammahgribi@gmail.com>
2022-12-10 15:45:10 +0700

    first commit

---
+++ src/main/java/co/id/mii/serversidekelompok3/ServersideKelompok3Application.java
@@ -0,0 +1,13 @@
+package co.id.mii.serversidekelompok3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServersideKelompok3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServersideKelompok3Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/id/mii/serversidekelompok3/ServersideKelompok3ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.mii.serversidekelompok3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServersideKelompok3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
